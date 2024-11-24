2e1a2cbe5ce50fd1c6a1d9bd098cbead02bf4d6d
Lucas <lucasemanuelndq@gmail.com>
2024-11-24 09:41:43 -0300

    app_recipes

---
+++ recipes/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def home(request):
+    return HttpResponse('home 1')
+
+def contato(request):
+    return HttpResponse('contato 1')
+
+def sobre(request):
+    return HttpResponse('sobre 1')
