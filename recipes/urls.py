760184ba239b4b93560b72208f59042176ccbbcd
Lucas <lucasemanuelndq@gmail.com>
2025-01-08 00:19:35 -0300

    Staticfiles ok, path recipes não ok

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,8 +1,8 @@
-from django.contrib import admin
 from django.urls import path
-from recipes.views import home
+
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', home),
+    path('', views.home),
+    path('recipes/<int:id>/', views.recipe)
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,8 +1,9 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'recipes/pages/home.html')
+    return render(request, 'recipes/pages/home.html', context={'name':'Lucas'})
 
+def recipe(request, id):
+    return render(request, 'recipes/pages/home.html', context={'name':'Lucas'})
 
