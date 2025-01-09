00523e0b47621236ed5e4ba4281fbb045b896524
Lucas <lucasemanuelndq@gmail.com>
2025-01-08 23:46:25 -0300

    templates_herança

--- django_projeto/settings.py
+++ django_projeto/settings.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'django_projeto.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'base_templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,6 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_DIRS = [
+    BASE_DIR / 'base_static',
+]
 
 STATIC_ROOT = BASE_DIR / 'static'
 
