be64ed7f8751625d44e90dd2c4679ab01375d5a6
Lucas <lucasemanuelndq@gmail.com>
2025-01-26 23:10:01 -0300

    404 search test

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,4 @@
+from django.http.response import Http404
 from django.shortcuts import get_list_or_404, get_object_or_404, render
 
 from .models import Recipe
@@ -27,4 +28,9 @@ def recipe(request, id):
 
 
 def search(request):
+    search_term = request.GET.get('search')
+
+    if not search_term:
+        raise Http404()
+    
     return render(request, 'recipes/pages/search.html')
