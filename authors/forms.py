bdc1e84abd9c40170e658ad2a95eb88548f9266a
Lucas <lucasemanuelndq@gmail.com>
2025-02-13 19:28:40 -0300

    labels e widgets forms

--- authors/forms.py
+++ authors/forms.py
@@ -5,4 +5,37 @@ from django.contrib.auth.models import User
 class RegisterForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ['first_name', 'last_name', 'username', 'email', 'password']
+        fields = [
+            'first_name', 
+            'last_name',
+            'username', 
+            'email', 
+            'password'
+            ]
+        #exclude = ['first_name']
+        labels = {
+            'username': 'Username',
+            'first_name': 'First name',
+            'last_name': 'Last name',
+            'email': 'E-mail',
+            'password': 'Password'
+        }
+
+        help_texts =  {
+            'email': 'The e-mail must be valid.'
+        }
+        error_messages = {
+            'username': {
+                'required': 'This field must not be empty',
+                'invalid': 'This field is invalid',
+            }
+        }
+
+        widgets = {
+            'username': forms.TextInput(attrs={
+                'placeholder': 'Type your username here'
+            }),
+            'password': forms.PasswordInput(attrs={
+                'placeholder': 'Type your password here'
+            })
+        }
