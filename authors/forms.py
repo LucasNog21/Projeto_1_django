541c6e75e8fb17d546e9198763086c2591913690
Lucas <lucasemanuelndq@gmail.com>
2025-02-13 19:59:43 -0300

    exemplo clean_field

--- authors/forms.py
+++ authors/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
 
 def add_attr(field, attr_name, attr_new_val):
@@ -76,3 +77,15 @@ class RegisterForm(forms.ModelForm):
                 'placeholder': 'Type your password here'
             })
         }
+    
+    def clean_password(self):
+        data = self.cleaned_data.get('password')
+
+        if 'atenção' in data:
+            raise ValidationError(
+                'Não digite %(value)s no campo password',
+                code='invalid',
+                params={'value': 'atenção'}
+            )
+
+        return data
