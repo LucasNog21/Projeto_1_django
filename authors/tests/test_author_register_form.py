2f94e5639ed6754a04dd109855e6f30617d93d62
Lucas <lucasemanuelndq@gmail.com>
2025-02-15 21:43:52 -0300

    testando placeholders

---
+++ authors/tests/test_author_register_form.py
@@ -0,0 +1,21 @@
+from django.test import TestCase
+from parameterized import parameterized
+
+from authors.forms import RegisterForm
+
+
+class AuthorRegisterForUnitTest(TestCase):
+    @parameterized.expand([
+        ('username','Your username'),
+        ('email','Your e-mail'),
+        ('first_name','Ex.: John'),
+        ('last_name','Ex.: Doe'),
+        ('password','Type your password'),
+        ('password2','Repeat your password'),
+    ])
+
+    def test_placeholder_is_correct(self, field, placeholder):
+        form = RegisterForm()
+        current_placeholder = form[field].field.widget.attrs['placeholder']
+
+        self.assertEqual(placeholder , current_placeholder)
